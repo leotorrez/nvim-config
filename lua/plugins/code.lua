29755318f58f833d2ff7db980a901626f4333857
Leo <leotorrez@gmail.com>
2025-06-17 01:32:03 -0300

    ADD: lsp configurations for ruff, venv selector FIX: wrong configurations for blender plugin

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -1,26 +1,32 @@
 return {
-    "b0o/blender.nvim",
-    lazy = true,
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "grapp-dev/nui-components.nvim",
-        "mfussenegger/nvim-dap", -- Optional, for debugging with DAP
-        "LiadOz/nvim-dap-repl-highlights", -- Optional, for syntax highlighting in the DAP REPL
+    {
+        "b0o/blender.nvim",
+        config = function()
+            require("blender").setup()
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "grapp-dev/nui-components.nvim",
+            "mfussenegger/nvim-dap", -- Optional, for debugging with DAP
+            "LiadOz/nvim-dap-repl-highlights", -- Optional, for syntax highlighting in the DAP REPL
+        },
     },
     {
-        "neolooong/whichpy.nvim",
-        opts = {
-            uv = { display_name = "uv" },
-        },
+        "linux-cultist/venv-selector.nvim",
+        dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+        event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     },
     {
         "you-n-g/jinja-engine.nvim",
     },
     {
-        "conform.nvim",
+        "neovim/nvim-lspconfig",
         opts = {
-            formatters_by_ft = {
-                ["python"] = { "black" },
+            servers = {
+                pyright = {
+                    mason = false,
+                    autostart = false,
+                },
             },
         },
     },
